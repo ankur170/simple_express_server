512fcc1f2a2aeabba78bcb964933e04523b3df74
Ankur <luckyyadav0017@gmail.com>
2019-11-25 17:19:26 +0530

    code c 1

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express=require('express')
 const app= express()
-app.get('/mc',(req,res)=>{
-    res.send('start hua bsdk')
+app.get('/api/timestamp/:date_string?',(req,res)=>{
+    let paramdata=req.params.date_string
+    let dat=new Date(paramdata)
+    let k={}
+    k.unix=dat.getTime()
+    k.utc=dat.toUTCString()
+    res.send(JSON.stringify(k))
+    
+
+
 })
 
 
-app.listen(process.env.PORT,()=>{console.log('lauda lelo bhosdike')})
+app.listen(3334,()=>{console.log('lauda lelo bhosdike')})

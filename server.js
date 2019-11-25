16eb293214725d228dbeea00911fa2016360937e
Ankur <luckyyadav0017@gmail.com>
2019-11-25 18:11:39 +0530

    code c2

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const express=require('express')
 const app= express()
 app.get('/api/timestamp/:date_string?',(req,res)=>{
     let paramdata=req.params.date_string
-    let dat=new Date(paramdata)
+    let dat
+    if(isNaN(paramdata)){
+         dat= new Date(paramdata)
+    }
+    else{
+         dat =new Date(parseInt(paramdata))
+    }
+        
     let k={}
     k.unix=dat.getTime()
     k.utc=dat.toUTCString()
-    res.send(JSON.stringify(k))
+    res.json((k))
     
 
 

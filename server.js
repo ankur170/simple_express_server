4e39c6b1670effa221385645cfea3d51b42d56a6
Ankur <luckyyadav0017@gmail.com>
2019-11-26 15:01:20 +0530

    'deploying'

--- server.js
+++ server.js
@@ -20,4 +20,4 @@ app.get('/api/timestamp/:date_string?',(req,res)=>{
 })
 
 
-app.listen(process.env.PORT,()=>{console.log('lauda lelo bhosdike')})
+app.listen(process.env.PORT,()=>{console.log('lauda lelo bsdk')})
